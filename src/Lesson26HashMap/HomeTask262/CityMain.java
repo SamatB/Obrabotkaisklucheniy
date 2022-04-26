6db46c206e229ca4039fd539222095f3052354b2
samat <samhardy1927@gmail.com>
2022-04-26 14:30:16 +0300

    LessonTask26-2

--- src/Lesson26HashMap/HomeTask262/CityMain.java
+++ src/Lesson26HashMap/HomeTask262/CityMain.java
@@ -1,12 +1,11 @@
 package Lesson26HashMap.HomeTask262;
 
 import java.util.HashSet;
-import java.util.Set;
 import java.util.TreeSet;
 
 public class CityMain {
     public static void main(String[] args) {
-        Set<City> cities2 = new TreeSet<>();
+//        Set<City> cities2 = new TreeSet<>();
         HashSet<City> cities = new HashSet<>();
         cities.add(new City(1, "New-York"));
         cities.add(new City(2, "Moscow"));
