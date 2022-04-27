4d13dc25f8d279f23f9246ab159edce57b0e9bcb
samat <samhardy1927@gmail.com>
2022-04-27 13:03:53 +0300

    HomeTask27

---
+++ src/Lesson27Generic/HomeTask/Box.java
@@ -0,0 +1,43 @@
+package Lesson27Generic.HomeTask;
+
+public class Box <T>{
+    private T height;
+    private T width;
+    private T depth;
+
+    public Box(T height, T width, T depth) {
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+
+    public Box() {
+    }
+
+    public T getHeight() {
+        return height;
+    }
+
+    public void setHeight(T height) {
+        this.height = height;
+    }
+
+    public void setWidth(T width) {
+        this.width = width;
+    }
+
+    public void setDepth(T depth) {
+        this.depth = depth;
+    }
+
+    @Override
+    public String toString() {
+        return "Box: " +
+                "height = " + height + " cm" +
+                ", width = " + width + " cm" +
+                ", depth = " + depth + " cm";
+    }
+    public static  <T> void generic(Box arg){
+        System.out.println(arg);
+    }
+}
---
+++ src/Lesson27Generic/HomeTask/GenMain.java
@@ -0,0 +1,8 @@
+package Lesson27Generic.HomeTask;
+
+public class GenMain {
+    public static void main(String[] args) {
+        Box.generic(new Box(12,23,34));
+        Box.generic(new Box(23,23,23));
+    }
+}
